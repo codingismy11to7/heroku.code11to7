8d4c211a61296a27c3cccab46dc703bbba4cd986
Steven Scott <steven@codemettle.com>
2014-05-23 19:08:21 -0400

    env

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,31 @@
 package controllers
 
 import play.api._
+import play.api.libs.concurrent.Akka
 import play.api.mvc._
+import reactivemongo.api.{MongoDriver, MongoConnection}
+import reactivemongo.api.collections.default.BSONCollection
+import play.api.Play.current
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+
+import scala.util.{Success, Failure}
 
 object Application extends Controller {
+    lazy val mongoDriver = new MongoDriver(Akka.system)
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
-  }
+    lazy val mongoDb = {
+        val connstr = sys.env getOrElse ("MONGOHQ_URL", sys.error("MONGOHQ_URL not set"))
+
+        MongoConnection.parseURI(connstr) match {
+            case Failure(t) ⇒ sys.error(s"$connstr invalid: $t")
+            case Success(u) ⇒ mongoDriver connection u db u.db.get
+        }
+    }
+
+    def index = Action.async {
+        mongoDb.collectionNames map (cn ⇒ {
+            Ok(views.html.index(s"ReactiveMongo collections: $cn"))
+        })
+    }
 
 }
